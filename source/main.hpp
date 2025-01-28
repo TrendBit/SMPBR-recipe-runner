7ff90e7c239a04b0596297ca2841d93a72552b21
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-01-28 10:35:44 +0100

    main: new processing with event executed by planner

--- source/main.hpp
+++ source/main.hpp
@@ -1,10 +1,20 @@
-#include "tools/httplib.h"
-#include <yaml-cpp/yaml.h>
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include <string>
-#include <nlohmann/json.hpp>
-#include <iomanip>  // Add at the top with other includes
+/**
+ * @file main.hpp
+ * @author Petr Malaník (TheColonelYoung(at)gmail(dot)com)
+ * @version 0.1
+ * @date 23.01.2025
+ */
 
-using json = nlohmann::json;
+#include <iostream>
+#include <string>
+
+#include "planner.hpp"
+
+#include "events/api_call.hpp"
+#include "events/api_get.hpp"
+#include "events/api_post.hpp"
+#include "events/bottle_temperature.hpp"
+#include "events/heater_intensity.hpp"
+#include "events/heater_temperature.hpp"
+#include "events/print.hpp"
+#include "events/wait.hpp"
