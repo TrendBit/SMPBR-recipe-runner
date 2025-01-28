b4f9ca5eba5ecdd2df250e4b98374e02d3bf0f9e
Petr Malanik <Malanik.Petr.gm@gmail.com>
2025-01-28 18:29:24 +0100

    planner: add loading of events from event list generated by config

--- source/planner.cpp
+++ source/planner.cpp
@@ -1,13 +1,89 @@
 #include "planner.hpp"
 
-void Planner::Start() {
+void Planner::Start(){
     for (auto &event : events_initialization) {
         event->Execute();
     }
 
-    while (true) {
+    while (not stop_flag) {
         for (auto &event : events_loop) {
+            if (stop_flag) {
+                break;
+            }
             event->Execute();
         }
     }
+
+    Clean_events();
+    std::cout << "Planner finished" << std::endl;
+}
+
+uint Planner::Load_init(std::vector<Events::Type> events, Config::Components components_settings){
+    uint count = 0;
+    for (const auto &event_type : events) {
+        try {
+            std::cout << "Generating init event: " << magic_enum::enum_name(event_type) << std::endl;
+            events_initialization.emplace_back(Generate_event(event_type, components_settings));
+            count++;
+        } catch (const std::runtime_error &e) {
+            std::cerr << "Failed to generate event: " << e.what() << std::endl;
+        }
+    }
+    return count;
+}
+
+uint Planner::Load_loop(std::vector<Events::Type> events, Config::Components components_settings){
+    uint count = 0;
+    for (const auto& event_type : events) {
+        try {
+            std::cout << "Generating loop event: " << magic_enum::enum_name(event_type) << std::endl;
+            events_loop.emplace_back(Generate_event(event_type, components_settings));
+            count++;
+        } catch (const std::runtime_error& e) {
+            std::cerr << "Failed to generate event: " << e.what() << std::endl;
+        }
+    }
+    return count;
+}
+
+std::unique_ptr<Events::Event> Planner::Generate_event(Events::Type type, Config::Components components_settings){
+    switch (type) {
+        case Events::Type::heater_on:
+            return std::make_unique<Events::Heater_intensity>(components_settings.heater.intensity);
+
+        case Events::Type::illumination_on: {
+            return std::make_unique<Events::LED_panel_intensity>(
+                0, components_settings.led_panel_channels[0]);
+        }
+        case Events::Type::mix:
+            return std::make_unique<Events::Mixer_stir>(
+                components_settings.mixer.rpm,
+                components_settings.mixer.length);
+
+        case Events::Type::air:
+            return std::make_unique<Events::Aerator_move>(
+                components_settings.aerator.volume);
+
+        case Events::Type::pump:
+            return std::make_unique<Events::Cuvette_pump_move>(
+                components_settings.cuvette_pump.volume);
+
+        case Events::Type::wait:
+            return std::make_unique<Events::Wait>(components_settings.cycle_length);
+
+        default:
+            throw std::runtime_error("Unknown event type");
+    }
+}
+
+void Planner::Clean_events(){
+    events_initialization.clear();
+    events_loop.clear();
+    Events::LED_panel_intensity(0, 0).Execute();
+    Events::Heater_turn_off().Execute();
+}
+
+void Planner::Stop(){
+    stop_flag = true;
+    Events::Wait::Stop();
 }
--- source/planner.hpp
+++ source/planner.hpp
@@ -10,7 +10,21 @@
 #include <vector>
 #include <memory>
 
+#include "config.hpp"
 #include "events/event.hpp"
+#include "events/types.hpp"
+
+#include "events/wait.hpp"
+#include "events/print.hpp"
+#include "events/bottle_temperature.hpp"
+#include "events/heater_intensity.hpp"
+#include "events/heater_temperature.hpp"
+#include "events/heater_turn_off.hpp"
+#include "events/led_panel_intensity.hpp"
+#include "events/cuvette_pump_move.hpp"
+#include "events/mixer_stir.hpp"
+#include "events/aerator_move.hpp"
+
 
 /**
  * @brief   Class responsible for planning and executing events
@@ -30,6 +44,11 @@ private:
      */
     std::vector<std::unique_ptr<Events::Event>> events_loop;
 
+    /**
+     * @brief   Signal handler can set this exit processing loop
+     */
+    std::atomic<bool> stop_flag{false};
+
 public:
     /**
      * @brief Construct a new Planner
@@ -46,6 +65,10 @@ public:
      */
     void Start();
 
+    uint Load_init(std::vector<Events::Type> events, Config::Components components_settings);
+
+    uint Load_loop(std::vector<Events::Type> events, Config::Components components_settings);
+
     /**
      * @brief   Add event to loop
      *
@@ -54,7 +77,7 @@ public:
      * @return int      Number of events in loop
      */
     template<typename T>
-    int Add(T&& event) {
+    int Add_loop(T&& event) {
         static_assert(std::is_base_of<Events::Event, std::remove_reference_t<T>>::value,
                      "T must derive from Event");
         events_loop.push_back(
@@ -63,4 +86,28 @@ public:
         return events_loop.size();
     }
 
+    /**
+     * @brief   Add event to initialization
+     *
+     * @tparam T        Type of event, deduced from passed argument
+     * @param event     Event which will be added to initialization
+     * @return int      Number of events in initialization
+     */
+    template<typename T>
+    int Add_initialization(T&& event) {
+        static_assert(std::is_base_of<Events::Event, std::remove_reference_t<T>>::value,
+                     "T must derive from Event");
+        events_initialization.push_back(
+            std::make_unique<std::remove_reference_t<T>>(std::forward<T>(event))
+        );
+        return events_initialization.size();
+    }
+
+    void Stop();
+
+private:
+
+    std::unique_ptr<Events::Event> Generate_event(Events::Type type, Config::Components components_settings);
+
+    void Clean_events();
 };
